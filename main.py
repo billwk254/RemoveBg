a195b1b867a203a432d57aa1de96e640044f79f6
billwk254 <billkihungu51@gmail.com>
2023-01-16 17:11:25 +0300

    Simple Python script to remove an image background

---
+++ main.py
@@ -0,0 +1,24 @@
+from rembg import remove
+from PIL import Image
+import easygui as eg
+import os
+from pathlib import Path
+
+try:
+    image_location = eg.fileopenbox(title="Open Image file")
+    input = Image.open(image_location)
+    input_path = os.path.abspath(image_location)
+    rgb_im = input.convert('RGB')
+    new_file = os.path.basename(input_path)
+    new_file = os.path.splitext(new_file)[0]
+    output = remove(rgb_im)
+    bg_removed = eg.filesavebox(title="Save file to", default=f'{Path.home() / "Pictures"}\\{new_file}-removedBG.png')
+    if bg_removed:
+        print(f"File was saved to {os.path.abspath(bg_removed)}")
+    output.save(bg_removed)
+
+except KeyboardInterrupt as e:
+    print("Program KeyboardInterrupt\n ")
+
+except AttributeError:
+    print("No image was Selected \n")
